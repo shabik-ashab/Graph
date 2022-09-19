7fb0d3c50432520eec877020a6c8fe558c0254ed
nub69 <shabik.ashab@gmail.com>
2022-09-19 14:06:41 +0600

    theory for graph added

---
+++ graphTh.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // we use graph to store relational data set
+
+    // difference between tree and graph
+    // every tree is graph but not every graph is tree
+    /*
+    tree is a acyclic ds              graph can be acyclic or cyclic
+    */
+
+    // node, edge
+    // bidirectional direction
+    // non weighted edge, weighted edge
+    // 4 types of graph
+    //1. bi->nw
+    //2. d->nw
+    //3. bi->w
+    //4. d->w
+    
+    // complete graph -> every node is connected to every node (e = (n(n-1))/2)
+    // number of edges
+    // 1. dense graph (if the val of edge is close to complete graph edge)
+    // 2. sporse graph
+
+    // 
+
+
+
+    return 0;
+}
